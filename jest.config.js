7d3147d47340eb11ef65adfff69a1f362fde4e56
Raphael Flechtner <raphael@kilt.io>
2023-04-27 11:55:17 +0200

    chore: prepare publishing package

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,11 @@
 module.exports = {
-  preset: 'ts-jest/presets/js-with-ts',
+  preset: 'ts-jest',
   testEnvironment: 'node',
   clearMocks: true,
   // Parachain block time is 12s
   testTimeout: 12000,
-  transformIgnorePatterns: [
-    '/node_modules/(?!@polkadot|@babel/runtime/helpers/esm/)',
-  ],
   collectCoverageFrom: ['**/*/src/**/*.ts'],
-  resolver: 'ts-jest-resolver',
   rootDir: 'src',
   coverageDirectory: 'coverage',
-  moduleDirectories: ['node_modules', '/src'],
+  moduleDirectories: ['node_modules'],
 }
