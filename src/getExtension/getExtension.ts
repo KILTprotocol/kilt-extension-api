b89d5ca2735b47f15eed6891a1760bdc6100a227
weichweich <albrecht@kilt.io>
2023-05-03 13:10:09 +0200

    feat: watch for extensions

--- src/getExtension/getExtension.ts
+++ src/getExtension/getExtension.ts
@@ -9,7 +9,7 @@ const apiWindow = window as Window & ApiWindow
 
 /**
  * Get all extensions that are currently initialized and support the Credential API.
- * 
+ *
  * Note that this method only returns the extensions that are initialized at the time when this function is called.
  * If an extension injects itself only after this function is called, it will not be contained in the returned extensions.
  * @returns an object containing extensions
@@ -19,6 +19,28 @@ export function getExtensions(): Record<string, InjectedWindowProvider<PubSubSes
   // copy all extensions into a new object since the caller should be allowed to change the object
   // without changing the underlying extension object.
   return { ...apiWindow.kilt }
+}
+
+/**
+ * Watch for new extensions that get injected.
+ *
+ * Each time an extension has injected itself, it will dispatch an event.
+ * This function calls the provided callback with all available extensions when such an event is received.
+ *
+ * NOTE: Use the returned cleanup function to remove the event listener when the callback is not needed anymore.
+ *
+ * @param callback Callback that gets called each time a new extension is injected.
+ * @returns Cleanup function which removes the listener for new extensions.
+ */
+export function watchExtensions(callback: (extensions: Record<string, InjectedWindowProvider<PubSubSessionV1 | PubSubSessionV2>>) => void): () => void {
+  function handler() {
+    callback(getExtensions())
+  }
+
+  window.addEventListener('kilt-extension#initialized', handler)
+  return () => {
+    window.removeEventListener('kilt-extension#initialized', handler)
+  }
 }
 
 /**
