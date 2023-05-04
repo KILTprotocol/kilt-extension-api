2e5538a85a9155d83cd0abfe4303e0e474022fa7
Albrecht <albrecht@kilt.io>
2023-05-04 13:41:44 +0200

    feat: watch for extensions (#13)
    # related https://github.com/KILTprotocol/ticket/issues/2126
    
    Adds a function to listen for new extensions.
    This can get merged once the Credential API supports these events.

--- src/getExtension/getExtension.ts
+++ src/getExtension/getExtension.ts
@@ -21,6 +21,33 @@ export function getExtensions(): Array<InjectedWindowProvider<PubSubSessionV1 | 
   return Object.values(apiWindow.kilt)
 }
 
+export type WatchExtensionsCallback = (extensions: Array<InjectedWindowProvider<PubSubSessionV1 | PubSubSessionV2>>) => void
+
+/**
+ * @private
+ * This function relies on API that is not part of the Credential API standard yet and might break at any point in time.
+ *
+ * Watch for new extensions that get injected.
+ *
+ * Each time an extension has injected itself, it will dispatch an event.
+ * This function calls the provided callback with all available extensions when such an event is received.
+ *
+ * NOTE: Use the returned cleanup function to remove the event listener when the callback is not needed anymore.
+ *
+ * @param callback Callback that gets called each time a new extension is injected.
+ * @returns Cleanup function which removes the listener for new extensions.
+ */
+export function watchExtensions(callback: WatchExtensionsCallback): () => void {
+  function handler() {
+    callback(getExtensions())
+  }
+
+  window.addEventListener('kilt-extension#initialized', handler)
+  return () => {
+    window.removeEventListener('kilt-extension#initialized', handler)
+  }
+}
+
 /**
  * This function enables the communication with extensions supporting the Credential API.
  *
