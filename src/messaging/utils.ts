b5042c7bc9ec8410b2c7087a22791c3884a5e34c
Adel Golghalyani <ad96el@gmail.com>
2023-08-09 09:58:20 +0200

    chore: copyright

--- src/messaging/utils.ts
+++ src/messaging/utils.ts
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2018-2023, BOTLabs GmbH.
+ *
+ * This source code is licensed under the BSD 4-Clause "Original" license
+ * found in the LICENSE file in the root directory of this source tree.
+ */
+
 import { ICType } from '@kiltprotocol/types'
 import { IMessage, MessageBody } from '../types'
 import { CType, SDKErrors } from '@kiltprotocol/sdk-js'
