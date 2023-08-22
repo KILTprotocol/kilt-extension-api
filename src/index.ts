22073b287c2e5ef8d5d4a2f07201f65861ff04ab
Adel Golghalyani <48685760+Ad96el@users.noreply.github.com>
2023-08-22 15:01:46 +0200

    namespace
    Co-authored-by: Raphael Flechtner <39338561+rflechtner@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export { getExtensions, watchExtensions, initializeKiltExtensionAPI } from './getExtension'
-export * from './messaging'
-export * from './quote'
+export * as Message from './messaging'
+export * as Quote from './quote'
