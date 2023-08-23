b5316c59a2e5c29e62eecdad852eda451d6abb89
Adel Golghalyani <ad96el@gmail.com>
2023-08-23 16:23:58 +0200

    remove comments

--- src/messaging/index.ts
+++ src/messaging/index.ts
@@ -10,17 +10,6 @@
  *
  * All messages are **encrypted** with the encryption keys of the involved identities.
  * Messages are encrypted using authenticated encryption: the two parties authenticate to each other, but the message authentication provides repudiation possibilities.
- *
- * The [[Message]] class exposes methods to construct and verify messages.
- *
- * @module @kiltprotocol/messaging
- */
-
-/**
- * Copyright (c) 2018-2023, BOTLabs GmbH.
- *
- * This source code is licensed under the BSD 4-Clause "Original" license
- * found in the LICENSE file in the root directory of this source tree.
  */
 
 export { fromBody } from './utils'
