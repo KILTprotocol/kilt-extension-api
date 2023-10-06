f0f307d9cc0de47f25bfd37551aad23887b30bdf
Adel Golghalyani <ad96el@gmail.com>
2023-10-06 14:47:15 +0200

    merge with develop

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,1 @@
-export * as Message from './messaging/index.js'
-export * as Quote from './quote/index.js'
-
 export { getExtensions, watchExtensions, initializeKiltExtensionAPI } from './getExtension/index.js'
