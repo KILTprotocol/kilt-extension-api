271277c47c3be6630ca019c7df666201503090c9
Adel Golghalyani <ad96el@gmail.com>
2023-10-10 11:13:27 +0200

    fix jest

--- jest.config.cjs
+++ jest.config.cjs
@@ -2,12 +2,17 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   clearMocks: true,
+    resolver: 'ts-jest-resolver',
   // Parachain block time is 12s
   testTimeout: 12000,
   collectCoverageFrom: ['**/*/src/**/*.ts'],
   rootDir: 'src',
   coverageDirectory: 'coverage',
-  moduleDirectories: ['node_modules'],
-  moduleFileExtensions: ['js', 'ts', 'tsx', 'json', 'node'],
-  modulePaths: ['<rootDir>'],
+  moduleDirectories: [ 'dist', 'node_modules'],
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.cjs.json'
+    }
+  },
+ 
 }
