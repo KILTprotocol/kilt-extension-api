51c64334c56242613872ec398ae48a9c0c718530
Raphael Flechtner <39338561+rflechtner@users.noreply.github.com>
2023-12-19 14:11:28 +0100

    chore: update license (#29)

--- src/getExtension/index.ts
+++ src/getExtension/index.ts
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2018-2023, Built on KILT.
+ *
+ * This source code is licensed under the BSD 4-Clause "Original" license
+ * found in the LICENSE file in the root directory of this source tree.
+ */
+
 import type { ApiWindow, InjectedWindowProvider, PubSubSessionV1, PubSubSessionV2 } from '../types/index.js'
 
 // cross-environment reference to global object (aka 'window' in browser environments)
--- src/messaging/interface/misc/index.ts
+++ src/messaging/interface/misc/index.ts
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2018-2023, Built on KILT.
+ *
+ * This source code is licensed under the BSD 4-Clause "Original" license
+ * found in the LICENSE file in the root directory of this source tree.
+ */
+
 import { Did } from '@kiltprotocol/sdk-js'
 
 import { IEncryptedMessage, IError, IReject, ISession } from '../../../types/index.js'
