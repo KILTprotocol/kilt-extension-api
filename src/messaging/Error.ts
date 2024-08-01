bb39a13512eab143a48a086be6cf28e283d97409
MarkusGarmeister <markusgarmeister@gmail.com>
2024-08-01 10:03:51 +0200

    fixed prettier format errors with linting

--- src/messaging/Error.ts
+++ src/messaging/Error.ts
@@ -1,11 +1,11 @@
 /**
- * Copyright (c) 2018-2024, Built on KILT.
+ * Copyright (c) 2018-2023, Built on KILT.
  *
  * This source code is licensed under the BSD 4-Clause "Original" license
  * found in the LICENSE file in the root directory of this source tree.
  */
 
-import { SDKErrors } from "@kiltprotocol/sdk-js";
+import { SDKErrors } from '@kiltprotocol/sdk-js'
 
 export class UnknownMessageBodyTypeError extends SDKErrors.SDKError {}
 export class DecodingMessageError extends SDKErrors.SDKError {}
@@ -13,7 +13,7 @@ export class CTypeUnknownPropertiesError extends SDKErrors.SDKError {}
 export class KeyError extends SDKErrors.SDKError {}
 export class IdentityMismatchError extends SDKErrors.SDKError {
   constructor(context?: string, type?: string) {
-    super(`Identity mismatch${context ? ` in context: ${context}` : ''}${type ? ` of type ${type}` : ''}`);
-    this.name = 'IdentityMismatchError';
+    super(`Identity mismatch${context ? ` in context: ${context}` : ''}${type ? ` of type ${type}` : ''}`)
+    this.name = 'IdentityMismatchError'
   }
 }
